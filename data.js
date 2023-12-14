7070d594cffd473c90d4304df229abfeb73e09e7
biyalas <130562162+biyalas@users.noreply.github.com>
2023-12-14 21:25:33 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+let songs = [
+    {
+        name: 'Be you',
+        path: 'musics/song1.mp3',
+        artist: 'Sam Smith',
+        cover: 'images/cover1.avif'
+    },
+    {
+        name: 'Christmas',
+        path: 'musics/song2.mp3',
+        artist: 'Thunders',
+        cover: 'images/cover2.jpg'
+    },
+    {
+        name: 'Hoping',
+        path: 'musics/song3.mp3',
+        artist: 'Suzen',
+        cover: 'images/cover3.webp'
+    }
+
+]
